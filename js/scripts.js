77e7ece47ffef23c6c24715277a958f37d4b686d
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-11-22 22:19:29 -0600

    Add ease

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function showLinks() {
   var links = document.getElementsByClassName("link");
-  TweenMax.staggerFromTo(links, 0.5, {opacity: 0, left: 50}, {opacity: 1, left: 0}, 0.2);
+  TweenMax.staggerFromTo(links, 0.5, {opacity: 0, left: 50}, {opacity: 1, left: 0, ease: Power2.easeOut}, 0.2);
 }
 
 window.onload = function() {
   var logo = document.getElementsByClassName("logo");
-  TweenLite.fromTo(logo, 1, {opacity: 0}, {opacity: 1, onComplete: showLinks});
+  TweenLite.fromTo(logo, 1, {opacity: 0}, {opacity: 1, ease: Power2.easeOut, onComplete: showLinks});
 }
