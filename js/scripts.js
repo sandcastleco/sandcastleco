4495bedaae03f2816f476fd088df55d8335272fe
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-11-22 23:11:33 -0600

    Add color change on hover

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,31 @@ function showLinks() {
 }
 
 window.onload = function() {
+  var body = document.getElementById("body");
+  var links = document.getElementsByClassName("link");
+  var falt = document.getElementById("flag");
+  var canvas = document.getElementById("canvas");
   var logo = document.getElementsByClassName("logo");
+
   TweenLite.fromTo(logo, 1, {opacity: 0}, {opacity: 1, ease: Power2.easeOut, onComplete: showLinks});
+
+  links[0].addEventListener("mouseover", function() {
+    TweenLite.to(flag, 0.3, {fill: "#D9482B"});
+  });
+  links[1].addEventListener("mouseover", function() {
+    TweenLite.to(flag, 0.3, {fill: "#30A7BF"});
+  });
+  links[2].addEventListener("mouseover", function() {
+    TweenLite.to(flag, 0.3, {fill: "#53A668"});
+  });
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("mouseout", function() {
+      TweenLite.to(flag, 0.3, {fill: "#FFF"});
+    });
+  }
+
+
+  /*body.addEventListener("click", function() {
+    canvas.innerHTML += '<div class="bubble"></div>';
+  });*/
 }
