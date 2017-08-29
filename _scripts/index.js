7bab2a366a55ef281a6e1a5f567c6028c6c6e8ae
Kevin McGillivray <kevin.mcgillivray@me.com>
2017-08-29 00:59:06 -0500

    Update projects layout

--- _scripts/index.js
+++ _scripts/index.js
@@ -1,15 +1,1 @@
-import mojs from 'mo-js';
-
-// console.log(mojs);
-//
-// var coverPage = document.getElementById('cover-page');
-// console.log(coverPage);
-//
-// var shape = new mojs.Shape({
-//   parent: coverPage,
-//   shape: 'circle',
-//   isShowStart: true
-//
-// });
-//
-// console.log(shape);
+var captions = document.getElementsByClassName('card-caption');
